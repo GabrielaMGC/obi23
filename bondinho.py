e17da5f603481e708a1cc8d816c3f83491c5ba65
GabrielaMGC <gmgc2010@gmail.com>
2023-05-12 15:22:26 -0300

    bondinho e morango

---
+++ bondinho.py
@@ -0,0 +1,12 @@
+P = int(input())
+A = int(input())
+
+S = A/49
+
+if S > P:
+    print("Não")
+
+Q = P+A 
+Y = Q/50
+print("Sim")
+print(Y)
---
+++ morango.py
@@ -0,0 +1,12 @@
+C1 = int(input())
+L1 = int(input())
+C2 = int(input())
+L2 = int(input())
+
+P = C1*L1
+S = C2*L2
+
+if P>S:
+    print(P)
+else:
+    print(S)
